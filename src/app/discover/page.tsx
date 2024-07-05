4683f7c649d8cb65509b353e7f1dc4996208f4b0
JeffreyJoel <jowoloko@gmail.com>
2024-07-05 16:19:58 +0100

    update discover page

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavBar } from "@/components/Navbar";
 import PostCard from "@/components/posts/PostsCard";
+import SeacrchBar from "@/components/shared/SearchBar";
 
 function Discover() {
   return (
@@ -10,10 +11,11 @@ function Discover() {
         <div className="main-layout__top"></div>
         <div className="main-layout__main">
           {/* <h1 className="text-center text-4xl mb-3 text-white">News</h1> */}
+         <div className="flex justify-center mb-8">
+         <SeacrchBar/>
+         </div>
           <div className="flex justify-center">
-            {/* <button class="px-4 py-2 bg-gray-200 hover:bg-gray-300 text-gray-800 text-sm font-medium rounded-full">
-	Default Button
-  </button> */}
+       
             <span className="inline-block px-5 py-2 bg-gray-200 hover:bg-gray-300 rounded-full text-md font-semibold text-gray-700 mr-2 mb-2 pointer">
               All
             </span>
